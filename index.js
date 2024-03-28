55d1e7cebe7ac03d7036628b9768bc58176bdf53
Elvis Moses Tangasaiti <elvis.moses@student.moringaschool.com>
2024-03-28 17:03:22 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+var mainElement = document.querySelector('main');
+if (mainElement) {mainElement.parentNode.removeChild(mainElement);} 
+const newHeader=document.createElement('h1');
+newHeader.id='victory';
+newHeader.innerHTML='Elvis Moses is the champion';
